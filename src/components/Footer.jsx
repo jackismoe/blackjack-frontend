12e68ed6121cc9a91d45a62095bcdc3618508b31
Jack <jacksonmorgan@hey.com>
2021-01-26 13:22:31 -0500

    rough outline of nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>
-        Footer
+      <div className='footer'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
       </div>
     )
   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Sling as Hamburger } from 'hamburger-react'
 
 export default class Nav extends React.Component {
   render() {
     return (
-      <div>
-        Nav
+      <div className='navBar'>
+        <div><Hamburger/></div>
+        <div><h3>reactJack</h3></div>
+        <div className='userIcon'>•</div>
       </div>
     )
   }
