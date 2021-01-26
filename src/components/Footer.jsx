ff5a91bc03cebd6ba4a8111464ac6eaa392c291e
Jack <jacksonmorgan@hey.com>
2021-01-26 13:31:31 -0500

    change footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     )
   }
